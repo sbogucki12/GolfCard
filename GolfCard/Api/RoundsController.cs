10452a65c4027732df8026c1deebc179355a86db
Steve Bogucki <sbogucki@mail.usf.edu>
2018-04-07 20:10:02 -0700

    tested web api controllers SUCCESS

--- GolfCard/Api/RoundsController.cs
+++ GolfCard/Api/RoundsController.cs
@@ -18,82 +18,85 @@ namespace GolfCard.Api
             _context = new GolfCardDbContext();
         }
 
-
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        // GET Current Golf Round
+        [HttpGet]
+        [Route("api/rounds/{roundName}")]
+        public GolfRound GetCurrentRound(string roundName)
         {
-            return new string[] { "value1", "value2" };
-        }
+            GolfRound currentRound = _context.GolfRounds.FirstOrDefault(g => g.RoundName == roundName);
 
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
+            return currentRound; 
         }
 
         // POST 
         // Player starts round by submitting par for the course
-        // A PUT action (AddHole) will take in hole by hole details
-        [HttpPost]
+        // A PUT action (AddHole) will take in hole by hole details        
         [Route("api/rounds/startround")]
-        public IHttpActionResult StartNewRound([FromBody]GolfRound golfRound)
-        {
-
-            GolfRound currentRound = _context.GolfRounds.FirstOrDefault(g => g.RoundName == golfRound.RoundName);
-
-           
-
-            GolfRound submittedGolfRound = new GolfRound
-            {
-                GolferName = golfRound.GolferName,
-                CourseName = golfRound.CourseName,
-                RoundPar = golfRound.RoundPar
-            };
-
-            _context.GolfRounds.Add(submittedGolfRound);
-            _context.SaveChanges();
-            return Ok("Info for Hole Submitted"); 
-
-        }
-
-        // PUT 
-        // Player starts round by submitting par for the course
-        // Another POST action will take in hole by hole details
-        [Route("api/rounds/addhole/{")]
-        public IHttpActionResult AddHole([FromBody]GolfRound golfRound)
+        public IHttpActionResult StartRound([FromBody]GolfRound golfRound)
         {
             GolfRound submittedGolfRound = new GolfRound
             {
                 GolferName = golfRound.GolferName,
                 CourseName = golfRound.CourseName,
+                RoundName = golfRound.RoundName,
                 RoundPar = golfRound.RoundPar
             };
 
             _context.GolfRounds.Add(submittedGolfRound);
             _context.SaveChanges();
             return Ok("Round Initiated");
-
         }
 
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        // PUT 
+        // Player starts round by submitting par for the course
+        // Another POST action will take in hole by hole details
+        [HttpPut]
+        [Route("api/rounds/addhole")]
+        public IHttpActionResult AddHole([FromBody]GolfRound golfRound)
         {
-        }
 
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
-        }
+            GolfRound currentRound = _context.GolfRounds.FirstOrDefault(g => g.RoundName == golfRound.RoundName);
 
-        public decimal GetRoundTotal()
-        {
-            throw new NotImplementedException();
-        }
+            currentRound.Hole1Par = golfRound.Hole1Par;
+            currentRound.Hole1Score = golfRound.Hole1Score;
+            currentRound.Hole2Par = golfRound.Hole2Par;
+            currentRound.Hole2Score = golfRound.Hole2Score;
+            currentRound.Hole3Par = golfRound.Hole3Par;
+            currentRound.Hole3Score = golfRound.Hole3Score;
+            currentRound.Hole4Par = golfRound.Hole4Par;
+            currentRound.Hole4Score = golfRound.Hole4Score;
+            currentRound.Hole5Par = golfRound.Hole5Par;
+            currentRound.Hole5Score = golfRound.Hole5Score;
+            currentRound.Hole6Par = golfRound.Hole6Par;
+            currentRound.Hole6Score = golfRound.Hole6Score;
+            currentRound.Hole7Par = golfRound.Hole7Par;
+            currentRound.Hole7Score = golfRound.Hole7Score;
+            currentRound.Hole8Par = golfRound.Hole8Par;
+            currentRound.Hole8Score = golfRound.Hole8Score;
+            currentRound.Hole9Par = golfRound.Hole9Par;
+            currentRound.Hole9Score = golfRound.Hole9Score;
+            currentRound.Hole10Par = golfRound.Hole10Par;
+            currentRound.Hole10Score = golfRound.Hole10Score;
+            currentRound.Hole11Par = golfRound.Hole11Par;
+            currentRound.Hole11Score = golfRound.Hole11Score;
+            currentRound.Hole12Par = golfRound.Hole12Par;
+            currentRound.Hole12Score = golfRound.Hole12Score;
+            currentRound.Hole13Par = golfRound.Hole13Par;
+            currentRound.Hole13Score = golfRound.Hole13Score;
+            currentRound.Hole14Par = golfRound.Hole14Par;
+            currentRound.Hole14Score = golfRound.Hole14Score;
+            currentRound.Hole15Par = golfRound.Hole15Par;
+            currentRound.Hole15Score = golfRound.Hole15Score;
+            currentRound.Hole16Par = golfRound.Hole16Par;
+            currentRound.Hole16Score = golfRound.Hole16Score;
+            currentRound.Hole17Par = golfRound.Hole17Par;
+            currentRound.Hole17Score = golfRound.Hole17Score;
+            currentRound.Hole18Par = golfRound.Hole18Par;
+            currentRound.Hole18Score = golfRound.Hole18Score;
 
-        public decimal CalculateRoundScore()
-        {
-            throw new NotImplementedException();
+            _context.SaveChanges();
+            return Ok("Info for Hole Submitted"); 
         }
+        
     }
 }
---
+++ README.md
@@ -0,0 +1,123 @@
+# GolfCard
+
+
+## Summary
+
+App for golfers.  It's a virtual scorecard that allows golfers to record and save their golf scores as they play. Views optimized for mobile (though not a native mobile app).  
+
+## Background
+
+I really like the technology stack that we use at work.  So, I wanted to build something with some of those same technologies.  At work, we use: 
+
++ C#
++ Redis
++ SignalR
++ ODATA 
++ .NET Web API
++ .NET MVC
++ React 
++ Redux
++ TypeScript
+
+This app, **GolfCard**, won't be quite so ambitious.  It's technology stack will be: 
+
++ C#
++ SQL Server Express
++ Entity Framework
++ LINQ (Maybe ODATA, if appropriate)
++ .NET Web API
++ NET MVC (home View will be root for the React App)
++ React
++ Redux
++ TypeScript
+
+I *REALLY* enjoy using TypeScript with React!!! 
+
+
+## Upcoming To-Dos 
+
+[ ] - Exception Handling in the Web APi Controller 
+
+[ ] - Scaffold out React/Redux app using Create React App with TypeScript 
+
+
+
+## Timeline
+
+**Day One (April 7, 2018)** 
+
++ Created data model
++ Created Web API
++ Created Controllers for Starting a Golf Round, Adding Hole Score, Getting a Golf Round
++ Created SQL Server Express Database
+
+
+## Credit
+
+- **Steve Bogucki**
+
+~~ISO FTE dev position in the Los Angeles area. Would consider relocation, but only to Minneapolis (weird, I know, but I grew up in the area).~~  
+
+My strength is C# fundamentals, .NET MVC and Web API, including all related technology: Entity Framework, SQL Server, LINQ etc.  I've deployed apps to Azure, Heroku, and AWS with Angular and AngularJS front-ends, but I intend to relearn vanilla JavaScript from the ground-up, just as I have C#.  
+
+I've only recently been digging into React and TypeScript and enjoy them individually but LOVE them together.  Still making sense of Redux :) 
+
+Please check out my LinkedIn: 
+
+[LinkedIn](https://www.linkedin.com/in/sbogucki12/ "Steve's LinkedIn Profile")
+
+Email: sbogucki@mail.usf.edu
+
+
+## Screenshots
+
++ Day One (April 7, 2018):
+
+*Entity Framework Migration creating database:*
+
+![Imgur](https://i.imgur.com/l4ZRs42.jpg)
+
+
+*Initial Web API controller:* 
+
+![Imgur](https://i.imgur.com/YwjNYne.jpg)
+
+
+
+*Initiating a Round, via Web API controller (in Postman):*
+
+![Imgur](https://i.imgur.com/cmlfgRR.jpg)
+
+
+
+*We love status code: 200!:* 
+
+![Imgur](https://i.imgur.com/YTq6kOO.jpg)
+
+
+
+*Confirming that our round and hole 1 score were saved:* 
+
+![Imgur](https://i.imgur.com/qfQ5Tpt.jpg)
+
+
+
+*The initial database schema:* 
+
+![Imgur](https://i.imgur.com/J75nT4X.jpg)
+
+
+
+
+
+**Go Birds**
+
+![Imgur](https://i.imgur.com/O0Hxg8b.jpg)
+
+
+
+
+
+
+
+

d72be235b8a0fb615e0d42819a5b6be5f1a19c01
Steve Bogucki <sbogucki@mail.usf.edu>
2018-04-22 17:11:08 -0700

    building more of the architecture, adding components, and routing

--- gcfrontend/src/components/addRoundData.jsx
+++ gcfrontend/src/components/addRoundData.jsx
@@ -1,12 +1,51 @@
 ﻿import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import '../../src/App.css';
 
 export default class AddRoundData extends Component {
+    constructor(props, state) {
+        super(props)
+        this.state = {
+            isHolesSelected: this.props.isHolesSelected,
+            holes: this.props.holes
+        }
+    }
+
+    onClickNine() {
+        this.setState({
+            isHolesSelected: true, 
+            holes: 9
+        })
+    }
+
+    onClickEighteen() {
+        this.setState({
+            isHolesSelected: true,
+            holes: 18
+        })
+    }
+
     render() {
-        return (
-            <div>
-                <h1>Add Round Data</h1>
-            </div>
-        )
+        {
+            let content;
+            if (this.state.isHolesSelected == false) {
+                content = <div>
+                    <h4>Are you playing 9 or 18 holes?</h4>
+                    <div className="row">
+                        <div className="col-lg-6"><button className="btn btn-info" onClick={() => this.onClickNine()}>9</button></div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-6"><button className="btn btn-warning" onClick={() => this.onClickEighteen()}>18</button></div>
+                    </div>
+                </div>
+            } else if (this.state.isHolesSelected == true && this.state.holes == 9) {
+                content =
+                    <div><h2>Nine Holes</h2></div>
+            } else if (this.state.isHolesSelected == true && this.state.holes == 18) {
+                content = 
+                    <div><h2>18 Holes</h2></div>
+            }
+            return content;
+        } 
     }
 }
--- gcfrontend/src/components/newRound.jsx
+++ gcfrontend/src/components/newRound.jsx
@@ -15,6 +15,8 @@ export default class NewRound extends Component {
         }
     }
 
+    isHolesSelected = false;
+
     onSubmit() {
         this.setState({
             isRoundNamed: true
@@ -26,8 +28,7 @@ export default class NewRound extends Component {
             <Router>
                 <div>
                     {this.state.isRoundNamed ? <div> <p>You are playing ROUNDNAME</p><Link to="/addrounddata"><button className="btn btn-primary">Click to add par info for Round</button></Link></div> : <div><h2>Please give your round a name:</h2><h4><i>If you leave this round, you can return to it using this name.</i></h4><p><input /><button className="btn btn-primary" onClick={() => this.onSubmit()}>Save Name</button></p></div>}
-                    <Route path="/addrounddata" component={AddRoundData} />
-
+                    <Route path="/addrounddata" render={(props) => <AddRoundData {...props} isHolesSelected={false} />} />
                 </div>
             </Router>
         )
